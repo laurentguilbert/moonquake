7931270446a6d3050665a8633de85f5e44334729
gcalmettes <gcalmettes@idmog.com>
2022-10-02 20:56:02 +0200

    deploy: run.sh

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,6 +1,6 @@
 export DJANGO_SETTINGS_MODULE="project.settings_prod"
 
 python manage.py collectstatic --noinput
+python manage.py migrate
 
 gunicorn -c gunicorn-config.py
-#gunicorn project.wsgi:application --workers 4 --bind 0.0.0.0:8000
